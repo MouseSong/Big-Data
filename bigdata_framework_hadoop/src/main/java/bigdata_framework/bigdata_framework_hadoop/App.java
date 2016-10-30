02a1a283d678f40bf2a093418698bc85ada157f4
zhangdong <zhangdonginpeking@gmail.com>
2016-10-30 17:22:27 +0800

    add hadoop project

---
+++ bigdata_framework_hadoop/src/main/java/bigdata_framework/bigdata_framework_hadoop/App.java
@@ -0,0 +1,13 @@
+package bigdata_framework.bigdata_framework_hadoop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
